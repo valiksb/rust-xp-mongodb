a55396351f977efee8491cb374fa51525c0bf887
Valik Solorzano <valiksb@solorzanob.net>
2024-01-14 00:57:13 -0500

    chore: added support to pass mongodb url in commandline and list all databases

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,39 @@
 mod error;
 
+use clap::{arg, command, ArgAction};
+use mongodb::{Client, options::{ClientOptions, ResolverConfig}};
+
 pub use self::error::{Error, Result};
 
-fn main() -> Result<()> {
+#[tokio::main]
+async fn main() -> Result<()> {
     println!("Hello, this world!");
+
+    let matches = command!() // requires `cargo` feature
+        .next_line_help(true)
+        .arg(arg!(--mongodb_url <VALUE>).required(true).action(ArgAction::Set))
+        .get_matches();
+
+    println!(
+        "mongodb_url: {:?}",
+        matches.get_one::<String>("mongodb_url").expect("required")
+    );
+
+    let mongodb_url = matches.get_one::<String>("mongodb_url").unwrap();
+
+    // A Client is needed to connect to MongoDB:
+    // An extra line of code to work around a DNS issue on Windows:
+    let options =
+        ClientOptions::parse_with_resolver_config(&mongodb_url, ResolverConfig::cloudflare())
+            .await?;
+    let client = Client::with_options(options)?;
+
+    // Print the databases in our MongoDB cluster:
+    println!("Databases:");
+    for name in client.list_database_names(None, None).await? {
+        println!("- {}", name);
+    }
+//    let client = Client::with_uri_str(mongodb_url)?;
+
     Ok(())
 }
